d0c258927786b0b503502fdb614a5e1f16d47714
Henry Blanchette <henry@monkeyface.com>
2024-07-18 15:29:09 -0400

    restructured to use classes

--- combinator.py
+++ combinator.py
@@ -86,6 +86,17 @@ class Combinator(ABC):
     def __call__(self, seq: Seq) -> Any:
         self.apply(seq)
 
+class Composition(Combinator):
+    description = """
+    Composition of a sequence of combinators.
+    """.strip()
+    
+    def __init__(self, cs: list[Combinator]):
+        super().__init__(
+            "; ".join([ c.name for c in cs ]),
+            fold_thn([ c.apply for c in cs ])
+        )
+
 class Translate(Combinator):
     description = """
     Translates the sequence to the right by `n`. Wraps.
@@ -96,13 +107,6 @@ class Translate(Combinator):
             f"translate(n={n})",
             bijective_puz(lambda s, i: s[(i - n) % seq_len])
         )
-
-def translate(n: int) -> Puz:
-    """
-    Translates the sequence to the right by `n`.
-    Wraps.
-    """
-    return bijective_puz(lambda s, i: s[(i - n) % seq_len])
 
 class Reflect(Combinator):
     description = """
@@ -115,12 +119,6 @@ class Reflect(Combinator):
             bijective_puz(lambda s, i: s[(-(i - i_pivot) + i_pivot) % seq_len])
         )
 
-def reflect(i_pivot: int) -> Puz:
-    """
-    Reflects the sequence about the index `i_pivot`. Wraps.
-    """
-    return bijective_puz(lambda s, i: s[(-(i - i_pivot) + i_pivot) % seq_len])
-
 class Colorshift(Combinator):
     description = """
     Adds `n` to the color of each non-background element.
@@ -132,15 +130,10 @@ class Colorshift(Combinator):
             bijective_puz(lambda s, i: s[i] + n if s[i] != bg else s[i])
         )
 
-def colorshift(n: int) -> Puz:
-    """
-    Adds `n` to the color of each non-background element.
-    """
-    return bijective_puz(lambda s, i: s[i] + n if s[i] != bg else s[i])
-
-class Colorshift(Combinator):
+class Expand(Combinator):
     description = """
-    Adds `n` to the color of each non-background element.
+    Expand each value in the input sequence to fill each pixel within `n` of it
+    in the output sequence.
     """.strip()
     
     def __init__(self, n: int):
@@ -159,25 +152,6 @@ class Colorshift(Combinator):
                 for i in range(seq_len)
             ]
         )
-
-def expand(n: int) -> Puz:
-    """
-    Expand each value in the input sequence to fill each pixel within `n` of it
-    in the output sequence.
-    """
-    def mode_non_bg(s: Seq):
-        """
-        Returns the most common non-`bg` value in `s`.
-        If `s` is all `bg`, then return `bg`.
-        """
-        counts: list[tuple[Val, int]] = [ (x, s.count(s)) for x in set(s) if x != bg ]
-        counts.sort(key=lambda x_count: x_count[1])
-        return counts[0][0] if len(counts) != 0 else bg
-        
-    return lambda s: [
-        mode_non_bg([ s[j % seq_len] for j in range(i - n, i + n + 1) ])
-        for i in range(seq_len)
-    ]
 
 # ==============================================================================
 # main
@@ -210,27 +184,43 @@ if __name__ == "__main__":
             bg
             for i in range(seq_len)
         ])
-    
-    puzzles: dict[str, tuple[Puz, Callable[[], Seq]]] = {
-        "translate":                        (fold_thn([translate(4)]),                                gen_some_blocks),
-        "reflect":                          (fold_thn([reflect(seq_len//2)]),                         gen_some_blocks),
-        "colorshift":                       (fold_thn([colorshift(2)]),                               gen_some_blocks),
-        "translate; reflect":               (fold_thn([translate(4), reflect(seq_len//2)]),           gen_some_blocks),
-        "translate; colorshift":            (fold_thn([translate(4), colorshift(2)]),                 gen_some_blocks),
-        "expand":                           (fold_thn([expand(1)]),                                   gen_some_pixels),
-        "expand; colorshift":               (fold_thn([expand(1), colorshift(2)]),                    gen_some_pixels),
-        "expand; translate":                (fold_thn([expand(1), translate(1)]),                     gen_some_pixels),
-    }
+
+    puzzles: list[Puz, Callable[[], Seq]] = [
+        (   Composition([ Translate(4) ]), 
+            gen_some_blocks
+        ),
+        (   Composition([ Reflect(seq_len//2) ]), 
+            gen_some_blocks
+        ),
+        (   Composition([ Colorshift(2) ]),
+            gen_some_blocks
+        ),
+        (   Composition([ Translate(4), Reflect(seq_len//2) ]),
+            gen_some_blocks
+        ),
+        (   Composition([ Translate(4), Colorshift(2) ]),
+            gen_some_blocks
+        ),
+        (   Composition([ Expand(1)]),
+            gen_some_pixels
+        ),
+        (   Composition([ Expand(1), Colorshift(2) ]),
+            gen_some_pixels
+        ),
+        (   Composition([ Expand(1), Translate(1) ]),
+            gen_some_pixels
+        ),
+    ]
 
     datasets = {}
-    for name, (puz, gen) in puzzles.items():
+    for (comb, gen) in puzzles:
         inputs, outputs = [], []
         for _ in range(num_samples):
             input = gen()
-            output = puz(input)
+            output = comb.apply(input)
             inputs.append(input)
             outputs.append(output)
         inputs_tensor, outputs_tensor = torch.tensor(inputs), torch.tensor(outputs)
-        datasets[name] = TensorDataset(inputs_tensor, outputs_tensor)
+        datasets[comb.name] = TensorDataset(inputs_tensor, outputs_tensor)
 
     visualize_datasets(datasets, grid_width=4, grid_height=2, num_samples=num_samples)
