76f039dd05d41eaad8362fe8f9e3dbda25d6ce9e
josh <freckletonj@gmail.com>
2024-07-25 01:27:15 -0700

    refactor into a `functional` paradigm

--- combinator.py
+++ combinator.py
@@ -1,360 +1,165 @@
-from abc import ABC, abstractmethod
-import torch
-from torch.utils.data import TensorDataset
-import matplotlib.colors as mcolors
-import matplotlib.pyplot as plt
+from typing import Callable, List, Any
 import random
-from random import randrange, choice, randint, shuffle, sample
-from typing import Any, Callable
+from torch.utils.data import TensorDataset
+import torch
 
 from visualization import visualize_datasets
 
-random.seed(777)
 
-# ==============================================================================
-# parameters
-# ==============================================================================
+SequenceTransformer = Callable[[List[Any]], List[Any]]
 
-Val = int
-bg: Val = 0 # background value
 
-Seq = list[Val] # type of sequences
-seq_len: int = 48
+def compose_transformers(transformers: List[SequenceTransformer]) -> SequenceTransformer:
+    """ Compose multiple sequence transformers into a single transformer. """
+    def composed_transformer(seq: List[int]) -> List[int]:
+        for transformer in transformers:
+            seq = transformer(seq)
+        return seq
+    return composed_transformer
 
-Puz = Callable[[Seq], Seq] # type of puzzles
 
-'''
-An idea for algorithmically creating new puzzles: define a collection of
-combinators that, when combinated in well-formed way, yield a puzzle.
+def create_transformer(func: Callable[[List[int], int], int]) -> SequenceTransformer:
+    """ Create a sequence transformer from a function that takes a sequence and an index as input. """
+    def transformer(seq: List[int]) -> List[int]:
+        return [func(seq, i) for i in range(len(seq))]
+    return transformer
 
-A *puzzle* in this context is a mapping `Seq -> Seq`. So if we have the
-combinators
-    - `x : A -> Seq -> Seq`,
-    - `y : B -> Seq -> Seq`, and
-    - `z : C -> Seq -> Seq`
-then we can construct the puzzle `lambda s: z(c, y(b, x(a, s)))`.
 
-I'm imagining that the combinators will be common sorts of transformations. But
-is of course an interesting question of how to classify terms of `Seq -> Seq`
-into "good puzzles" and "bad puzzles".
-'''
+def translate(n: int) -> SequenceTransformer:
+    """ Translate the sequence by n positions. """
+    return create_transformer(lambda seq, i: seq[(i - n) % len(seq)])
 
-# ==============================================================================
-# utilities
-# ==============================================================================
 
-def bg_seq() -> Seq:
-    return [bg] * seq_len
+def reflect(i_pivot: int) -> SequenceTransformer:
+    """ Reflect the sequence about the index i_pivot. """
+    return create_transformer(lambda seq, i: seq[(-(i - i_pivot) + i_pivot) % len(seq)])
 
-def thn(g: Callable, f: Callable) -> Callable:
-    """
-    g then f
-    """
-    return lambda x: f(g(x))
 
-def fold_thn(fs: list[ Callable[[Any], Any] ]) -> Callable[[Any], Any]:
-    """
-    f_1 then ... then f_n i.e.
-        fold_thn([f_1, ..., f_n]) = lambda x: f_n(... f_1(x))
+def colorshift(n: int) -> SequenceTransformer:
+    """ Shift the color of each element in the sequence by n. """
+    return create_transformer(lambda seq, i: seq[i] + n if seq[i] != 0 else seq[i])
 
-    Note that
-        fold_thn([]) = lambda x: x
-    """
-    if len(fs) == 0:
-        return lambda x: x
-    else:
-        return thn(fs[0], fold_thn(fs[1:]))
 
-def bijective_puz(f: Callable[[Seq, int], Val]) -> Puz:
-    return lambda s: [ f(s, i) for i in range(seq_len) ]
+def shrink(seq: List[int]) -> List[int]:
+    """For each span of consecutive instances of a specific value, map the
+    midpoint of the range to that value and the rest of the range to 0."""
+    new_seq = [0] * len(seq)
+    spans = []
+    current_span = {"start": 0, "val": seq[0], "len": 1}
 
-# ==============================================================================
-# combinators
-# ==============================================================================
+    for i, v in enumerate(seq[1:], 1):
+        if v == current_span["val"]:
+            current_span["len"] += 1
+        else:
+            spans.append(current_span)
+            current_span = {"start": i, "val": v, "len": 1}
+    spans.append(current_span)
 
-class Combinator(ABC):
-    description: str
-    
-    def __init__(self, name: str, apply: Puz):
-        self.name_ = name
-        self.apply = apply
+    for span in spans:
+        mid = span["start"] + span["len"] // 2
+        new_seq[mid % len(seq)] = span["val"]
 
-    @property
-    def name(self):
-        return self.name_
+    return new_seq
 
-    def __call__(self, seq: Seq) -> Any:
-        self.apply(seq)
-        
-    @classmethod
-    def sample(cls): 
-        raise Exception()
 
-class Composition(Combinator):
-    description = """
-    Composition of a sequence of combinators.
-    """.strip()
-    
-    def __init__(self, cs: list[Combinator]):
-        super().__init__(
-            "; ".join([ c.name for c in cs ]),
-            fold_thn([ c.apply for c in cs ])
-        )
-        
-    @classmethod
-    def sample(cls):
-        return Composition([])
+def expand(n: int) -> SequenceTransformer:
+    """Expand each value in the input sequence to fill each pixel within `n` of it
+    in the output sequence. """
+    def transformer(seq: List[int]) -> List[int]:
+        def mode_non_bg(s: List[int]):
+            counts = [(x, s.count(x)) for x in set(s) if x != 0]
+            counts.sort(key=lambda x_count: x_count[1], reverse=True)
+            return counts[0][0] if counts else 0
 
-class Translate(Combinator):
-    description = """
-    Translates the sequence to the right by `n`. Wraps.
-    """.strip()
-
-    def __init__(self, n: int):
-        super().__init__(
-            f"translate(n={n})",
-            translate(n)
-        )
-        
-    @classmethod
-    def sample(cls):
-        return cls(random.randint(-2, 2))
-
-def translate(n: int) -> Puz:
-    return bijective_puz(lambda s, i: s[(i - n) % seq_len])
-
-class Reflect(Combinator):
-    description = """
-    Reflects the sequence about the index `i_pivot`. Wraps.
-    """.strip()
-    
-    def __init__(self, i_pivot: int):
-        super().__init__(
-            f"reflect(i_pivot={i_pivot})",
-            bijective_puz(lambda s, i: s[(-(i - i_pivot) + i_pivot) % seq_len])
-        )
-        
-    @classmethod
-    def sample(cls):
-        return cls(seq_len//2)
-
-class Colorshift(Combinator):
-    description = """
-    Adds `n` to the color of each non-background element.
-    """.strip()
-    
-    def __init__(self, n: int):
-        super().__init__(
-            f"colorshift(n={n})",
-            bijective_puz(lambda s, i: s[i] + n if s[i] != bg else s[i])
-        )
-
-    @classmethod        
-    def sample(cls):
-        return cls(random.sample([-1, 2, 3], 1)[0])
-
-class Expand(Combinator):
-    description = """
-    Expand each value in the input sequence to fill each pixel within `n` of it
-    in the output sequence.
-    """.strip()
-    
-    def __init__(self, n: int):
-        def mode_non_bg(s: Seq):
-            """
-            Returns the most common non-`bg` value in `s`.
-            If `s` is all `bg`, then return `bg`.
-            """
-            counts: list[tuple[Val, int]] = [ (x, s.count(s)) for x in set(s) if x != bg ]
-            counts.sort(key=lambda x_count: x_count[1])
-            return counts[0][0] if len(counts) != 0 else bg
-        super().__init__(
-            f"expand(n={n})",
-            lambda s: [
-                mode_non_bg([ s[j % seq_len] for j in range(i - n, i + n + 1) ])
-                for i in range(seq_len)
-            ]
-        )
-        
-    @classmethod
-    def sample(cls):
-        return cls(random.randint(1, 4))
-
-class Shrink(Combinator):
-    description = """
-    For each span of consecutive instances of a specific value `v`, map the
-    midpoint of the range to `v` and the rest of the range to `bg`. rest of the
-    range mapped to `bg`.
-    """
-
-    def __init__(self):
-        def puz(s: Seq) -> Seq:
-            s_new = bg_seq()
-            # collect spans
-            spans: list[dict[str, Any]] = []
-            current_span = { "start": 0, "val": s[0], "len": 1 }
-            for (i, v) in enumerate(s[1:]):
-                if v == current_span["val"]:
-                    current_span["len"] += 1
-                else:
-                    spans.append(current_span)
-                    current_span = { "start": i, "val": v, "len": 1 }
-            # in `s_new`, set midpoint of each span to its val
-            for span in spans:
-                mid = span["start"] + span["len"]//2 + 1
-                s_new[mid % seq_len] = span["val"]
-            return s_new
-
-        super().__init__("shrink", puz)
-
-    @classmethod
-    def sample(cls):
-        return cls()
-
-class Endpoints(Combinator):
-    def __init__(self):
-        def puz(s: Seq) -> Seq:
-            s_new = bg_seq()
-            # collect spans
-            spans: list[dict[str, Any]] = []
-            current_span = { "start": 0, "val": s[0], "len": 1 }
-            for (i, v) in enumerate(s[1:]):
-                if v == current_span["val"]:
-                    current_span["len"] += 1
-                else:
-                    spans.append(current_span)
-                    current_span = { "start": i, "val": v, "len": 1 }
-            # in `s_new`, set endpoints of each span to its val
-            for span in spans:
-                end0 = span["start"]
-                s_new[end0 + 1 % seq_len] = span["val"]
-                end1 = span["start"] + span["len"]
-                s_new[end1 % seq_len] = span["val"]
-            return s_new
-        super().__init__("endpoints", puz)
-
-    @classmethod
-    def sample(cls):
-        return cls()
-
-# ==============================================================================
-# main
-# ==============================================================================
-
-if __name__ == "__main__":
-    num_samples = 10
-
-    def gen_some_pixels(colors=[1,2,3,4]) -> Seq:
-        random.shuffle(colors)
-        t = seq_len//4
-        n = random.randrange(t//2, t)
-        return translate(n)([
-            colors[0 % len(colors)] if i == 0*t else
-            colors[1 % len(colors)] if i == 1*t else
-            colors[2 % len(colors)] if i == 2*t else
-            colors[3 % len(colors)] if i == 3*t  else
-            bg
-            for i in range(seq_len)
-        ])
-
-    def gen_some_blocks(colors=[1,4]) -> Seq:
-        """
-        Randomly generate a sequence to use as input to a `Puz`.
-        """
-        random.shuffle(colors)
-        t = seq_len//6
-        n = random.randrange(t, 2*t)
-        return translate(n)([
-            colors[0 % len(colors)]   if i in range(0*t, 1*t) else
-            colors[1 % len(colors)]   if i in range(3*t, 4*t) else
-            bg
-            for i in range(seq_len)
-        ])
-        
-    def gen_one_block(color=2) -> Callable[[], Seq]:
-        def go():
-            t = seq_len//3
-            n = random.randrange(1*t, 1.5*t)
-            return translate(n)([
-                color   if i in range(0*t, 1*t) else
-                bg
-                for i in range(seq_len)
-            ])
-        return go
-
-    grid_width = 4
-    grid_height = 5
-
-    puzzles: list[Puz, Callable[[], Seq]] = []
-
-    autogen = True
-    if autogen:
-        for i in range(grid_width * grid_height):
-            c_options: list[Combinator] = [ 
-                Translate,
-                Reflect,
-                Colorshift,
-                Expand,
-                Shrink,
-                Endpoints
-            ]
-            random.shuffle(c_options)
-            cs = [ c.sample() for c in c_options[0:3] ]
-            # puzzles.append((Composition(cs), gen_some_blocks))
-            puzzles.append((Composition(cs), gen_one_block(color=random.randint(2, 6))))
-            
-    else:
-        puzzles = [
-            (   Composition([ Translate(4) ]), 
-                gen_some_blocks
-            ),
-            (   Composition([ Reflect(seq_len//2) ]), 
-                gen_some_blocks
-            ),
-            (   Composition([ Colorshift(2) ]),
-                gen_some_blocks
-            ),
-            (   Composition([ Translate(4), Reflect(seq_len//2) ]),
-                gen_some_blocks
-            ),
-            (   Composition([ Translate(4), Colorshift(2) ]),
-                gen_some_blocks
-            ),
-            (   Composition([ Expand(1)]),
-                gen_some_pixels
-            ),
-            (   Composition([ Expand(1), Colorshift(2) ]),
-                gen_some_pixels
-            ),
-            (   Composition([ Expand(1), Translate(1) ]),
-                gen_some_pixels
-            ),
-            (   Composition([ Shrink() ]),
-                gen_some_blocks
-            ),
-            (   Composition([ Shrink(), Expand(2) ]),
-                gen_some_blocks
-            ),
-            (   Composition([ Endpoints() ]),
-                gen_some_blocks
-            ),
-            (   Composition([ Endpoints(), Expand(1) ]),
-                gen_one_block()
-            ),
-            (   Composition([ Endpoints(), Expand(4), Endpoints(), Expand(1) ]),
-                gen_one_block()
-            ),
+        return [
+            mode_non_bg([seq[j % len(seq)] for j in range(i - n, i + n + 1)])
+            for i in range(len(seq))
         ]
 
-    datasets = {}
-    for (comb, gen) in puzzles:
-        inputs, outputs = [], []
-        for _ in range(num_samples):
-            input = gen()
-            output = comb.apply(input)
-            inputs.append(input)
-            outputs.append(output)
-        inputs_tensor, outputs_tensor = torch.tensor(inputs), torch.tensor(outputs)
-        datasets[comb.name] = TensorDataset(inputs_tensor, outputs_tensor)
+    return transformer
 
-    visualize_datasets(datasets, grid_width=grid_width, grid_height=grid_height, num_samples=num_samples)
+
+def endpoints(seq: List[int]) -> List[int]:
+    ''' Identify the start/end of each block '''
+    new_seq = [0] * len(seq)
+    spans = []
+    current_span = {"start": 0, "val": seq[0], "len": 1}
+    for i, v in enumerate(seq[1:]):
+        if v == current_span["val"]:
+            current_span["len"] += 1
+        else:
+            spans.append(current_span)
+            current_span = {"start": i + 1, "val": v, "len": 1}
+    spans.append(current_span)
+    for span in spans:
+        end0 = span["start"]
+        new_seq[end0 % len(seq)] = span["val"]
+        end1 = span["start"] + span["len"] - 1
+        new_seq[end1 % len(seq)] = span["val"]
+    return new_seq
+
+
+def gen_some_blocks(colors: List[int]) -> List[int]:
+    """ Generate a sequence of blocks with random colors. """
+    t = 48 // 6
+    n = random.randrange(t, 2 * t)
+    return translate(n)([
+        colors[0 % len(colors)] if i in range(0 * t, 1 * t) else
+        colors[1 % len(colors)] if i in range(3 * t, 4 * t) else
+        0
+        for i in range(48)
+    ])
+
+
+def gen_one_block(colors: int) -> List[int]:
+    """ Generate a sequence of a single block with a random color. """
+    t = 48 // 3
+    n = random.randrange(1 * t, 1.5 * t)
+    color = random.choice(colors)
+    return translate(n)([
+        color if i in range(0 * t, 1 * t) else
+        0
+        for i in range(48)
+    ])
+
+
+##########
+# Demo
+
+random.seed(42)
+
+colors = [1, 2, 3, 4, 6, 7, 8, 9]
+
+puzzles = [
+    ('translate(4)', translate(4), gen_some_blocks),
+    ('reflect(seq_len//2)', reflect(24), gen_some_blocks),
+    ('colorshift(2)', colorshift(2), gen_some_blocks),
+    ('translate(4) + reflect(seq_len//2)', compose_transformers([translate(4), reflect(24)]), gen_some_blocks),
+    ('translate(4) + colorshift(2)', compose_transformers([translate(4), colorshift(2)]), gen_some_blocks),
+    ('expand(1)', expand(1), gen_some_blocks),
+    ('expand(1) expand(1)', compose_transformers([expand(1), expand(1)]), gen_some_blocks),
+    ('expand(1) + colorshift(2)', compose_transformers([expand(1), colorshift(2)]), gen_some_blocks),
+    ('expand(1) + translate(1)', compose_transformers([expand(1), translate(1)]), gen_some_blocks),
+    ('shrink', shrink, gen_some_blocks),
+    ('shrink + expand(2)', compose_transformers([shrink, expand(2)]), gen_some_blocks),
+    ('endpoints', endpoints, gen_some_blocks),
+    ('expand(1) + endpoints', compose_transformers([expand(1), endpoints]), gen_one_block),
+    ('endpoints + expand(1)', compose_transformers([endpoints, expand(1)]), gen_one_block),
+    ('endpoints + expand(4) + endpoints + expand(1)', compose_transformers([endpoints, expand(4), endpoints, expand(1)]), gen_one_block)
+]
+
+datasets = {}
+num_samples = 10
+grid_width = 4
+grid_height = 5
+for (name, transformer, gen) in puzzles:
+    inputs, outputs = [], []
+    for _ in range(num_samples):
+        input = gen(colors)
+        output = transformer(input)
+        inputs.append(input)
+        outputs.append(output)
+        inputs_tensor, outputs_tensor = torch.tensor(inputs), torch.tensor(outputs)
+        datasets[name] = TensorDataset(inputs_tensor, outputs_tensor)
+
+visualize_datasets(datasets, grid_width=grid_width, grid_height=grid_height, num_samples=num_samples)
